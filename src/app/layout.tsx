8122047e479847e6b76fab23f5c6586635e3d774
XIE159 <XIE19113@email.com>
2026-08-21 12:41:52 +0500

    chore(analytics): remove GTM container GTM-PGNHTGZ5
    The shared container routes events to tracking.providentestate.com (invalid
    cert) and is also installed on unrelated wparena staging sites. This site
    must have no connection to it - all tracking scripts removed.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata, Viewport } from "next";
-import Script from "next/script";
 import "./provident.css";
 import "./header-styles.css";
 import "./developer-styles.css";
@@ -34,24 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head />
-      <body>
-        <Script
-          id="gtm"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PGNHTGZ5');`,
-          }}
-        />
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PGNHTGZ5"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
